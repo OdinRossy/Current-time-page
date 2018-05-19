c1836c8f6b7f9063364fe7325f8a68e084d28c3c
Gleb Remniov <OdinRossy@users.noreply.github.com>
2018-05-19 17:35:53 +0300

    include changing color

--- main.js
+++ main.js
@@ -1,24 +1,49 @@
+
 function currentTime() {
-    var date = new Date();
-    var hours = date.getHours();
-    var minutes = date.getMinutes();
-    var seconds = date.getSeconds();
-    var htime = document.getElementById("time");
-    var currentTime;
+    let date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    let htime = document.getElementById("time");
+    let currentTime;
     if (seconds<10){
         seconds = "0" + seconds;
     }
-
     if (minutes<10){
         minutes = "0" + minutes;
     }
     if (hours<10){
         hours = "0" + hours;
     }
-
     currentTime = hours + ":" + minutes + ":" + seconds;
-
     htime.innerText = currentTime;
-    console.log(currentTime);
+    // console.log(currentTime);
 }
 setInterval(currentTime, 500);
+
+let r = 255;
+let g = 0;
+let b = 0;
+let a = 0.8;
+function changeBackground() {
+    if (r===255 && b===0) {
+        g++;
+    }
+    if (g===255 && b===0) {
+        r--;
+    }
+    if (r===0 && g===255) {
+        b++;
+    }
+    if (r===0 && b===255) {
+        g--;
+    }
+    if (g===0 && b===255) {
+        r++;
+    }
+    if (r===255 && g===0) {
+        b--;
+    }
+    document.getElementById("header").style.backgroundColor = "rgba("+r+","+g+","+b+","+a+")";
+}
+setInterval(changeBackground, 25);

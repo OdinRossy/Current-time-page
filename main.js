a0db3f004966ee8c3403df82a4afb06e56301abe
Gleb Remniov <OdinRossy@users.noreply.github.com>
2018-05-19 15:27:03 +0300

    some minor fixes

--- main.js
+++ main.js
@@ -3,9 +3,15 @@ function currentTime() {
     var hours = date.getHours();
     var minutes = date.getMinutes();
     var seconds = date.getSeconds();
-    var htime = document.getElementById("time")
-    var currentTime = hours + ":" + minutes + ":" + seconds;
-    htime.innerHTML = currentTime;
+    var htime = document.getElementById("time");
+    var currentTime;
+    if (seconds<10){
+        currentTime = hours + ":" + minutes + ":0" + seconds;
+    }
+    else {
+        currentTime = hours + ":" + minutes + ":" + seconds;
+    }
+    htime.innerText = currentTime;
     console.log(currentTime);
 }
 setInterval(currentTime, 500);

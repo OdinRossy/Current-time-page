23654d7a4cfff5d5c714b8de5b83a2bdd1cf9356
Gleb Remniov <OdinRossy@users.noreply.github.com>
2018-05-24 21:38:08 +0300

    Adding jQuery

--- script/main.js
+++ script/main.js
@@ -3,7 +3,7 @@ function currentTime() {
     let hours = date.getHours();
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
-    let time = document.getElementById("time");
+    let time = $("#time");
     let currentTime;
     if (seconds < 10){
         seconds = "0" + seconds;
@@ -15,22 +15,20 @@ function currentTime() {
         hours = "0" + hours;
     }
     currentTime = hours + ":" + minutes + ":" + seconds;
-    time.innerText = currentTime;
-    // console.log(currentTime);
+    time.text(currentTime);
 }
 setInterval(currentTime, 500);
 
 let r = 255;    // Переменная для значения красного
 let g = 222;    // Переменная для значения зелёного
 let b = 0;      // Переменная для значения чёрного
-let a = 0.7;    // Переменная для значения прозрачности
-let isChangeBackground = false;
-let interval;
-let header_time = document.getElementById("header-text");
+let a = 0.0;    // Переменная для значения прозрачности
+let isChangeBackground = true;
+let interval = setInterval(changeBackground, 15);;
+let header_time = $("#header-text");
+let header_color;
 
-header_time.addEventListener("click", blockChangeBackground);
-
-function blockChangeBackground() {
+header_time.click(function () {
     isChangeBackground = !isChangeBackground;   //При вызове этой функции значение переменной isChangeBackground меняется на противоположное
     if (isChangeBackground){
         interval = setInterval(changeBackground, 15);  //Функция setInterval запускает функцию changeBackground каждые 15 миллисекунд
@@ -38,10 +36,13 @@ function blockChangeBackground() {
     else {
         clearInterval(interval); //Очищаем интервал, если значение isChangeBackground = false
     }
-}
+});
 
 function changeBackground() {   //Функция, в которой изменияем цвет фона документа
     if (isChangeBackground){
+        if (a<0.6){
+            a+=0.005;
+        }
         if (r===255 && b===0) {
             g++;
         }
@@ -61,6 +62,10 @@ function changeBackground() {   //Функция, в которой измени
             b--;
         }
         // Устанавливаем стиль backgroundColor элементу с id header-text
-        header_time.style.backgroundColor = "rgba(" + r + "," + g + "," + b + "," + a + ")";
+        header_color = "rgba(" + r + "," + g + "," + b + "," + a + ")";
+        header_time.css("background-color", header_color);
+        header_time.css("box-shadow", "0 5px 100px " + header_color)
+        // document.getElementById("header-text").style.backgroundColor = header_color;
+        // document.getElementById("header-text").style.boxShadow = header_color;
     }
 }
